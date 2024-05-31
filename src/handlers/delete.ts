65ef032004d1c7b0f9b5376d8409038bc8b18f33
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-05-31 22:47:03 +0800

    RM6785Bot: handlers/delete.ts: Take into account of @RM6785Bot postfix possibility

--- src/handlers/delete.ts
+++ src/handlers/delete.ts
@@ -6,7 +6,7 @@ const deleteHandler = async (ctx: Context<Update>): Promise<void> => {
   if (ctx.message === undefined) return;
   if (!("text" in ctx.message)) return;
 
-  let msgText: String = ctx.message.text.replace(/^\/delete/, "").trim();
+  let msgText: String = ctx.message.text.replace(/^\/delete(@RM6785Bot)?/, "").trim();
   let splitMsg: Array<String> = msgText.split("/");
   if (splitMsg.length === 0) {
     ctx.reply("Are you sure that's a valid link?");
