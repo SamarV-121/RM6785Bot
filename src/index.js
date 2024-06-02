22404f04dc246a695abc33fd331a0084f86797ea
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-06-02 13:00:39 +0800

    RM6785Bot: Split automatic post detection to a separate file

--- src/index.js
+++ src/index.js
@@ -117,63 +117,8 @@ bot.start((ctx) =>
   )
 );
 
-bot.on("message", async (ctx) => {
-  const { message } = ctx;
-
-  // Disable post auto detection in public groups
-  if (message.chat.type === "supergroup" && message.chat.username) {
-    return;
-  }
-
-  if (typeof message.caption === "undefined") return;
-
-  // recovery is currently not supported by linter,
-  // so let's not trigger the linter if # is recovery.
-  if (
-    message.caption.search("#ROM") !== -1 ||
-    message.caption.search("#KERNEL") !== -1
-  ) {
-    message.reply_to_message = {
-      caption: message.caption,
-      caption_entities: message.caption_entities,
-    };
-    if (message.chat.type === "private") {
-      const [lintResult, lintSuccessful] = require("./utils/lintUtils")(
-        message.reply_to_message.caption,
-        message.reply_to_message.caption_entities
-      );
-      ctx.replyWithHTML(lintResult, {
-        reply_to_message_id: ctx.message.reply_to_message.message_id,
-      });
-
-      if (lintSuccessful) {
-        const { MAX_REQUESTS, REQUEST_TIMEOUT } = require("./constants");
-        const userRequests = UserRequestUtils.updateUserRequest(ctx.chat.id);
-        if (userRequests > MAX_REQUESTS) {
-          ctx.reply(
-            `Spam detected, Try again after ${REQUEST_TIMEOUT / 60000} minutes`
-          );
-          return;
-        }
-
-        await ctx.telegram.forwardMessage(
-          TELEGRAM_RELEASE_CHAT,
-          message.chat.id,
-          message.message_id
-        );
-        ctx.replyToMessage("Forwarded post in the group for approval");
-        const updatedCtx = {
-          ...ctx,
-          chat: { ...ctx.chat, id: TELEGRAM_RELEASE_CHAT },
-        };
-
-        require("./handlers/lsauth").execute(updatedCtx);
-      }
-    } else {
-      linter.execute(ctx);
-    }
-  }
-});
+// set up automatic post detection
+require("./autoPostDetection")(bot);
 
 bot.launch();
 
@@ -181,6 +126,7 @@ module.exports = { bot };
 
 if (argv.ci) {
   console.log("Starting the bot with CI");
+  // eslint-disable-next-line global-require
   const commitListener = require("./ci");
 
   setInterval(commitListener, 5000);
