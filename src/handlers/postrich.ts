466a09fcacb31997e8791ae11cf23deaf22b71e0
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2026-06-21 06:33:29 +0800

    implement postRich. a LOT of error handling are still missing but this alone took me forever to write by hand and I'm now sleepy af.

---
+++ src/handlers/postrich.ts
@@ -0,0 +1,156 @@
+import type { BotContext, HandlerDescriptor } from "../types";
+import {
+  messageInfo,
+  hasEnoughVotes,
+  currentVotes,
+} from "../utils/messageUtils";
+import {
+  POST_TIMEOUT,
+  MAX_VOTES,
+  TELEGRAM_STICKER_FILE_ID,
+  TELEGRAM_RM6785_CHANNEL,
+  TELEGRAM_RM6785_CHAT,
+  TELEGRAM_R7_CHAT,
+} from "../constants";
+import { replyToMessage } from "../utils/contextUtils";
+import type { Message } from "node-telegram-bot-api";
+import { parsePost, constructPostRichBlock } from "../utils/postParser";
+
+const postrichHandler = async (ctx: BotContext) => {
+  if (!ctx.message.reply_to_message || !ctx.message.text) return;
+
+  const chatId = ctx.message.chat.id;
+  const messageId = ctx.message.reply_to_message.message_id;
+  const votes = currentVotes(messageId);
+  const mText = ctx.message.text?.split(" ");
+  if (mText.length < 3) {
+    await replyToMessage(ctx, "Not enough arg");
+    return;
+  }
+  const timeoutMatch = mText[1].match(/(\d+)m/);
+  let timeoutInMs = POST_TIMEOUT;
+  if (timeoutMatch) {
+    const timeoutInMinutes = parseInt(timeoutMatch[1]);
+    timeoutInMs = timeoutInMinutes * 60000;
+  }
+  const bannerLink = mText[2];
+
+  if (!messageInfo[messageId]) {
+    messageInfo[messageId] = {};
+  }
+
+  const msg = messageInfo[messageId];
+
+  if (msg.isPosted) {
+    await replyToMessage(
+      ctx,
+      "This message has already been scheduled for posting."
+    );
+    return;
+  }
+
+  // if (!hasEnoughVotes(messageId)) {
+  //   await replyToMessage(
+  //     ctx,
+  //     `This message does not have enough approvals (${votes}/${MAX_VOTES})`
+  //   );
+  //   return;
+  // }
+
+  msg.isPosted = true;
+
+  try {
+    const sentSticker = await ctx.bot.sendSticker(
+      TELEGRAM_RM6785_CHANNEL,
+      TELEGRAM_STICKER_FILE_ID
+    );
+
+    msg.stickerMessageId = sentSticker.message_id;
+
+    const sentMessage = await replyToMessage(
+      ctx,
+      `Scheduled to post in ${timeoutInMs / 60000}m`
+    );
+    const sentMessageId = sentMessage.message_id;
+
+    let secondsLeft = Math.floor(timeoutInMs / 1000);
+
+    const countdownTimeout = async (m: Message) => {
+      if (secondsLeft % 5 === 0) {
+        await ctx.bot.editMessageText(
+          `Scheduled to post in ${Math.floor(secondsLeft / 60)}m ${
+            secondsLeft % 60
+          }s`,
+          {
+            chat_id: chatId,
+            message_id: sentMessageId,
+          }
+        );
+      }
+
+      if (secondsLeft <= 0) {
+        console.log(m);
+        const parsed = parsePost(m);
+        if (!parsed) return;
+        const richMarkdown = constructPostRichBlock(parsed, bannerLink);
+
+        const sentPostMessage = await ctx.bot.sendRichMessage(
+          TELEGRAM_RM6785_CHANNEL,
+          {
+            markdown: richMarkdown,
+          }
+        );
+
+        msg.isPosted = false;
+
+        await ctx.bot.editMessageText("Posted successfully!", {
+          chat_id: chatId,
+          message_id: sentMessageId,
+        });
+
+        try {
+          const forwardAndPin = async (fromChat: number, toChat: number) => {
+            const forwardedMsg = await ctx.bot.forwardMessage(
+              toChat,
+              fromChat,
+              sentPostMessage.message_id
+            );
+            await ctx.bot.pinChatMessage(toChat, forwardedMsg.message_id);
+          };
+
+          await forwardAndPin(TELEGRAM_RM6785_CHANNEL, TELEGRAM_RM6785_CHAT);
+          await forwardAndPin(TELEGRAM_RM6785_CHANNEL, TELEGRAM_R7_CHAT);
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        msg.timeoutId = setTimeout(
+          countdownTimeout,
+          1000,
+          ctx.message.reply_to_message!
+        );
+      }
+
+      secondsLeft -= 1;
+    };
+
+    const timeoutId = setTimeout(
+      countdownTimeout,
+      1000,
+      ctx.message.reply_to_message!
+    );
+    msg.timeoutId = timeoutId;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const handler: HandlerDescriptor = {
+  command: "postrich",
+  help: "Publish an approved message on the channel.",
+  auth: true,
+  reply_to_message: true,
+  execute: postrichHandler,
+};
+
+export { handler };
---
+++ src/utils/postParser.ts
@@ -0,0 +1,324 @@
+import {
+  Message,
+  RichBlockParagraph,
+  RichBlockSectionHeading,
+  RichBlockList,
+  RichBlockPhoto,
+  RichBlock,
+  PhotoSize,
+  RichTextHashtag,
+  RichTextBold,
+  RichBlockListItem,
+} from "node-telegram-bot-api";
+
+export interface ParsedPostData {
+  buildAuthor: string;
+  buildDate: string;
+  buildAndroidVersion: string;
+  hashtags: string[];
+  title: string;
+  changelogs: string[];
+  bugs: string[];
+  notes?: string[];
+  downloads: string[];
+  downloadLink: string;
+  sourcesLink: string;
+  screenshotsLink: string;
+  supportgroupLink: string;
+}
+
+export const parsePost = (m: Message): ParsedPostData | undefined => {
+  if (!m.entities) return undefined;
+
+  const pp: ParsedPostData = {
+    buildAuthor: "",
+    buildDate: "",
+    buildAndroidVersion: "",
+    hashtags: [],
+    title: "",
+    changelogs: [],
+    bugs: [],
+    notes: [],
+    downloads: [],
+    downloadLink: "",
+    sourcesLink: "",
+    screenshotsLink: "",
+    supportgroupLink: "",
+  };
+
+  // find all inline links
+  let downloadLink: string = "";
+  let sourcesLink: string = "";
+  let screenshotsLink: string = "";
+  let supportgroupLink: string = "";
+  for (const entity of m.entities) {
+    if (entity.type != "text_link") continue;
+    const url = entity.url!;
+    const length = entity.length;
+    const offset = entity.offset;
+    const theText = m.text!.substring(offset, offset + length);
+    switch (theText) {
+      case "Download":
+        downloadLink = url;
+        break;
+      case "Sources":
+        sourcesLink = url;
+        break;
+      case "Screenshots":
+        screenshotsLink = url;
+        break;
+      case "Support group":
+        supportgroupLink = url;
+        break;
+      default:
+        console.warn(`[postParser] unknown text_link: ${theText}`);
+    }
+  }
+
+  pp.downloadLink = downloadLink;
+  pp.sourcesLink = sourcesLink;
+  pp.screenshotsLink = screenshotsLink;
+  pp.supportgroupLink = supportgroupLink;
+
+  const lines = m.text!.split("\n");
+  let inSection:
+    | ""
+    | "hashtags"
+    | "changelog"
+    | "bugs"
+    | "notes"
+    | "downloads" = "";
+  // parse line-by-line
+  for (const l of lines) {
+    if (l === "") continue;
+
+    if (l.startsWith("#")) {
+      inSection = "hashtags";
+      pp.hashtags = l.split(" ");
+      continue;
+    }
+
+    if (inSection === "hashtags") {
+      inSection = "";
+      pp.title = l;
+      continue;
+    }
+
+    if (l.startsWith("• Author")) {
+      pp.buildAuthor = l.split(":")[1].trim();
+      continue;
+    }
+    if (l.startsWith("• Android version")) {
+      pp.buildAndroidVersion = l.split(":")[1].trim();
+      continue;
+    }
+    if (l.startsWith("• Build date")) {
+      pp.buildDate = l.split(":")[1].trim();
+      continue;
+    }
+
+    if (l.startsWith("Changelog")) {
+      inSection = "changelog";
+      continue;
+    }
+    if (l.startsWith("Bugs")) {
+      inSection = "bugs";
+      continue;
+    }
+    if (l.startsWith("Notes")) {
+      inSection = "notes";
+      continue;
+    }
+    if (l.startsWith("Downloads")) {
+      inSection = "downloads";
+      continue;
+    }
+
+    if (
+      l.startsWith("Sources") ||
+      l.startsWith("Screenshots") ||
+      l.startsWith("Support group")
+    )
+      continue;
+
+    if (!l.startsWith("• ")) continue;
+    const lList = l.replace("• ", "");
+    switch (inSection) {
+      case "changelog":
+        pp.changelogs.push(lList);
+        break;
+      case "bugs":
+        pp.bugs.push(lList);
+        break;
+      case "notes":
+        pp.notes?.push(lList);
+        break;
+      case "downloads":
+        if (lList === "Download") continue;
+        pp.downloads.push(lList);
+        break;
+      // default:
+      //   throw new Error("unreachable");
+    }
+  }
+
+  if (pp.notes?.length == 0) delete pp.notes;
+  return pp;
+};
+
+export const constructPostRichBlock = (
+  pd: ParsedPostData,
+  bannerLink: string
+): string => {
+  return `\
+![](${bannerLink} "ROM banner")
+
+${pd.hashtags.join(" ")}
+
+# ${pd.title}
+
+- Author: ${pd.buildAuthor}
+- Android version: ${pd.buildAndroidVersion}
+- Build date: ${pd.buildDate}
+
+## Changelog
+
+- ${pd.changelogs.join("\n- ")}
+
+## Bugs
+
+- ${pd.bugs.join("\n- ")}
+
+## Notes
+
+- ${pd.notes?.join("\n- ")}
+
+## Downloads
+
+- ${pd.downloads.join("\n- ")}
+
+[Sources](${pd.sourcesLink})
+
+[Screenshots](${pd.screenshotsLink})
+
+[Support group](${pd.supportgroupLink})
+`;
+};
+
+/* export */ const _constructPostRichBlock = (
+  pd: ParsedPostData,
+  banner: PhotoSize
+): { blocks: RichBlock[] } => {
+  const changelogRichListItems = pd.changelogs.map((v) => {
+    return {
+      label: v,
+      blocks: [] as RichBlock[],
+    } as RichBlockListItem;
+  });
+  const bugsRichListItems = pd.bugs.map((v) => {
+    return {
+      label: v,
+      blocks: [] as RichBlock[],
+    } as RichBlockListItem;
+  });
+  const downloadsRichListItems = pd.downloads.map((v) => {
+    return {
+      label: v,
+      blocks: [] as RichBlock[],
+    } as RichBlockListItem;
+  });
+
+  // notes is optional, so it's handled differently
+  const notesBlock = [];
+  if (pd.notes) {
+    const notesRichListItems = pd.notes.map((v) => {
+      return {
+        label: v,
+        blocks: [] as RichBlock[],
+      } as RichBlockListItem;
+    });
+    notesBlock.push(
+      {
+        type: "heading",
+        text: { type: "bold", text: "Notes" } as unknown as RichTextBold,
+        size: 2,
+      } as RichBlockSectionHeading,
+      {
+        type: "list",
+        items: notesRichListItems,
+      } as RichBlockList
+    );
+  }
+
+  const built = {
+    blocks: [
+      {
+        type: "photo",
+        photo: [banner] as PhotoSize[],
+      } as RichBlockPhoto,
+      {
+        type: "paragraph",
+        text: pd.hashtags.join(" ") as unknown as RichTextHashtag,
+        size: 6,
+      } as RichBlockParagraph,
+      {
+        type: "heading",
+        text: { type: "bold", text: pd.title } as unknown as RichTextBold,
+        size: 1,
+      } as RichBlockSectionHeading,
+      {
+        type: "list",
+        items: [
+          {
+            label: `Author: ${pd.buildAuthor}`,
+            blocks: [] as RichBlock[],
+          } as RichBlockListItem,
+          {
+            label: `Android version: ${pd.buildAndroidVersion}`,
+            blocks: [] as RichBlock[],
+          } as RichBlockListItem,
+          {
+            label: `Build date: ${pd.buildDate}`,
+            blocks: [] as RichBlock[],
+          } as RichBlockListItem,
+        ],
+      } as RichBlockList,
+      {
+        type: "heading",
+        text: { type: "bold", text: "Changelog" } as unknown as RichTextBold,
+        size: 2,
+      } as RichBlockSectionHeading,
+      {
+        type: "list",
+        items: changelogRichListItems,
+      } as RichBlockList,
+      {
+        type: "heading",
+        text: { type: "bold", text: "Bugs" } as unknown as RichTextBold,
+        size: 2,
+      } as RichBlockSectionHeading,
+      {
+        type: "list",
+        items: bugsRichListItems,
+      } as RichBlockList,
+      ...notesBlock,
+      {
+        type: "heading",
+        text: { type: "bold", text: "Downloads" } as unknown as RichTextBold,
+        size: 2,
+      } as RichBlockSectionHeading,
+      {
+        type: "list",
+        items: [
+          ...downloadsRichListItems,
+          {
+            label: `[Download](${pd.downloadLink})`,
+            blocks: [] as RichBlock[],
+          } as RichBlockListItem,
+        ],
+      } as RichBlockList,
+    ],
+  };
+
+  return built;
+};
